90b2516be57eff2d4b4d31b71aa04148b5e0198c
Laura Pérez <87994943+LauraP30@users.noreply.github.com>
2021-09-09 13:20:07 -0500

    Código QR

---
+++ Pruebas Laura/Codigo QR.py
@@ -0,0 +1,26 @@
+f=[]
+b=[]
+e=[]
+import qrcode
+img = qrcode.make()
+print("__Menú central__")
+print("1) Menu del día y personalizar pedido")
+print("2) Reservación de dia para restaurante")
+print("3) Entrada y Salida de trabajadores")
+mnu1=int(input("Seleccione la opción de: "))
+for i in range(1,4):
+  if(mnu1==1):
+    f = open("Menu del Desayuno.png", "wb")
+    img.save(f)
+    f.close()
+  if(mnu1==2):
+    b = open("Reservación.png", "wb")
+    img.save(b)
+    f.close()
+  if(mnu1==3):
+    e = open("Empleados.png", "wb")
+    img.save(e)
+    f.close()
+f=[]
+b=[]
+e=[]
