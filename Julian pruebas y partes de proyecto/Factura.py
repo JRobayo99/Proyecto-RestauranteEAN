44339396a4b26cbb0e22d9b1deaa4d3d440de523
Julian Ramirez Robayo <jramire23664@universidadean.edu.co>
2021-09-15 17:04:30 -0500

    Proyecto. prototipos

--- Julian pruebas y partes de proyecto/Factura.py
+++ Julian pruebas y partes de proyecto/Factura.py
@@ -2,8 +2,8 @@ import tkinter as tk
 
 ventana= tk.Tk()
 ventana.title ("Restaurante ménu del día")
-ventana.geometry("700*500")
-ventana.configure(bg= "White")
+ventana.geometry("700x500")
+ventana.configure(bg= "white")
 
 cliente= tk.StringVar()
 ruc=tk.StringVar()
@@ -55,10 +55,9 @@ def calcular():
         resultado4.set(float(cantidad4.get())*float(precio4.get()))
         resultado5.set(float(cantidad5.get())*float(precio5.get()))
         resultado6.set(float(cantidad6.get())*float(precio6.get()))
-        resultadoT.set("Subtotal: "+str(float(resultado1.get())+float
-        (resultado2.get())+float(resultado3.get())+float(resultado3.get())+float(resultado4.get())+float(resultado5.get())+float(resultado6.get())))
+        resultadoT.set("Subtotal: "+str(float(resultado1.get())+float(resultado2.get())+float(resultado3.get())+float(resultado3.get())+float(resultado4.get())+float(resultado5.get())+float(resultado6.get())))
         iva.__set("Iva: "+str(0.12(float(resultado1.get())+float(resultado2.get())+float(resultado3.get())+float(resultado4.get())+float(resultado5.get())+float(resultado6.get()))))
-        resultadoTIVA.set("Total: "+str(1.12*(float(resultado1.get())+float(resultado2.get())+float(resultado3.get())+float(resultado4.get())+float(resultado5.get())+float(resultado1.get()))))
+        resultadoTIVA.set("Total: "+str(1.12*(float(resultado1.get())+float(resultado2.get())+float(resultado3.get())+float(resultado4.get())+float(resultado5.get())+float(resultado6.get()))))
 def limpiar():
     cliente.set("")
     ruc.set("")
@@ -95,8 +94,76 @@ def limpiar():
 
 tk.Label(text="FACTURA: )", bg="white").place(x=275,y=10)
 
-tk.Label(text="Cliente: ", bg= "while").place()
+tk.Label(text="Cliente: ", bg= "while").place(x=10, y=40)
+tk.Label(text="RUC",bg ="white" ).place(x=10, y=70)
 
 
+tk.Entry(textvariable= cliente,bd=1, width=45,justify="left").place(x=100,y=40)
+tk.Entry(textvariable= ruc,bd=1, width=45, justify="left").place(x=100, y=70)
 
-    
+tk.Button(text="limpiar",width=10,heigth=1, bg= "Orange", command=limpiar).place(x=570, y=40)
+tk.Button(text="calcular",width=10,height=1,bg="Orange", command=calcular).place(x=570,y=70)
+
+tk.Label(text="PRODUCTO ",bg= "white").place(x=100, y=100)
+tk.Label(text="CANTIDAD ",bg= "white").place(x=220, y=100)
+tk.Label(text="PRECIO U",bg="white").place(x=320,y=100)
+tk.Label(text="PRECIO T", bg="white").place(x=420, y=100)
+
+tk.Label(text="Jefes: el gordo",bg="white").place(x=10,y=460)
+
+tk.Entry(textvariable= producto1,bd=1,width=12,justify="left").place(x=90,y=130)
+
+tk.Entry(textvariable= producto2,bd=1,width=12,justify="left").place(x=90,y=160)
+
+tk.Entry(textvariable= producto3,bd=1,width=12,justify="left").place(x=90,y=190)
+
+tk.Entry(textvariable= producto4,bd=1,width=12,justify="left").place(x= 90, y=220)
+
+tk.Entry(textvariable= producto5,bd=1,width=12,justify="left").place(x= 90, y=250)
+
+tk.Entry(textvariable= producto6,bd=1,width=12,justify="left").place(x= 90, y=260)
+
+
+tk.Entry(textvariable= cantidad1,bd=1,width=5,justify="left").place(x=230,y=130)
+
+tk.Entry(textvariable= cantidad2,bd=1,width=5,justify="left").place(x=230,y=160)
+
+tk.Entry(textvariable= cantidad3,bd=1,width=5,justify="left").place(x=230,y=190)
+
+tk.Entry(textvariable= cantidad4,bd=1,width=5,justify="left").place(x=230,y=220)
+
+tk.Entry(textvariable= cantidad5,bd=1,width=5,justify="left").place(x=230,y=250)
+
+tk.Entry(textvariable= cantidad6,bd=1,width=5,justify="left").place(x=230,y=280)
+
+
+tk.Entry(textvariable= precio1,bd=1,width=8,justify="left").place(x=315,y=130)
+
+tk.Entry(textvariable= precio2,bd=1,width=8,justify="left").place(x=315,y=160)
+
+tk.Entry(textvariable= precio3,bd=1,width=8,justify="left").place(x=315,y=190)
+
+tk.Entry(textvariable= precio4,bd=1,width=8,justify="left").place(x=315,y=220)
+
+tk.Entry(textvariable= precio5,bd=1,width=8,justify="left").place(x=315,y=250)
+
+tk.Entry(textvariable= precio6,bd=1,width=8,justify="left").place(x=315,y=280)
+
+
+tk.Label(textvariable= resultado1, bg="white",bd=1,width= 8, justify="left").place(x=420,y=130)
+
+tk.Label(textvariable= resultado2, bg="white",bd=1,width= 8, justify="left").place(x=420,y=160)
+
+tk.Label(textvariable= resultado3, bg="white",bd=1,width= 8, justify="left").place(x=420,y=190)
+
+tk.Label(textvariable= resultado4, bg="white",bd=1,width= 8, justify="left").place(x=420,y=220)
+
+tk.Label(textvariable= resultado5, bg="white",bd=1,width= 8, justify="left").place(x=420,y=250)
+
+tk.Label(textvariable= resultado6, bg="white",bd=1,width= 8, justify="left").place(x=420,y=280)
+
+tk.Label(textvariable= resultadoT,justify="left").place(x=370,y=350)
+tk.Label(textvariable= iva,justify="left").place(x=370,y=380)
+tk.Label(texvariable= resultadoTIVA, justify="left").place(x=480,y=10)
+
+ventana.mainloop()
