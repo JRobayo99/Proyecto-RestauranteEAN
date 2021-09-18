ec028cec1d20d2b18c7744e2a0530850102b8b8b
juancamilo840 <88996180+juancamilo840@users.noreply.github.com>
2021-09-18 15:32:53 -0500

    Create menu_3.0.py

---
+++ menu_3.0.py
@@ -0,0 +1,20 @@
+menu_restaurante=int(input("menu:\n 1- bandeja paisa $8.000 \n 2- hamburguesa de res $12.000 \n 3- pepitoria $7.500 \n 4- mojarra frita $5.500 \n 5- lasagna $14.000 \n 6- jugos naturales $3.500 \n 7- casuela de mariscoz $10.500 \n 0-salir \n"))
+while menu_restaurante != 0:
+  if menu_restaurante == 1:
+    print("bandeja paisa$8.000")
+  elif menu_restaurante == 2:
+    print("hamburguesa de res$12.000")
+  elif menu_restaurante == 3:
+    print("pepitoria$7.500")
+  elif menu_restaurante == 4:
+    print("mojarra frita$5.500")
+  elif menu_restaurante == 5:
+    print("lasagna$14.000")
+  elif menu_restaurante == 6:
+    print("jugos naturales$3.500")
+  elif menu_restaurante == 7:
+    print("casuela de mariscoz$10.500")
+  else:
+    print("digitar una opcion correcta")
+
+  menu_restaurante=int(input("menu:\n 1- bandeja paisa $8.000 \n 2- hamburguesa de res $12.000 \n 3- pepitoria $7.500 \n 4- mojarra frita $5.500 \n 5-lasagna $14.000 \n 6-jugos naturales $3.500 \n 7-casuela de mariscoz $10.500 \n 0-salir \n"))
