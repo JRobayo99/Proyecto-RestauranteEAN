57e985e16625ca04e4cdb6134fd6c64739d3e611
Julian Ramirez Robayo <jramire23664@universidadean.edu.co>
2021-09-19 18:51:59 -0500

    Update Inventario.py

--- Prototipo de pruebas/Inventario.py
+++ Prototipo de pruebas/Inventario.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 import pymysql
 
 
+#cursor.execute("CREATE TABLE Inventario(id int PRIMARY KEY AUTO_INCREMENT NOT NULL, nombre_p VARCHAR(50), cantidad_c VARCHAR(50), clasifc_p VARCHAR(50), precio_u VARCHAR(50) NOT NULL)")
 bd=pymysql.connect(
     host="localhost",
     user="root",
@@ -14,7 +15,8 @@ bd=pymysql.connect(
 
 cursor = bd.cursor()
 
-cursor.execute("CREATE TABLE Inventario(Nombre del producto int PRIMARY KEY AUTO_INCREMENT NOT NULL, nombre_p VARCHAR(50), cantidad_c VARCHAR(50), clasifc_p VARCHAR(50), precio_u VARCHAR(50) NOT NULL)")
+def cerrar():
+    vnt.destroy()
 
 vnt=tk.Tk()
 vnt.title("Formulario de registro")
@@ -25,6 +27,12 @@ image=tk.PhotoImage(file="logo1.gif")
 image=image.subsample(3,3)
 label=tk.Label(image=image)
 label.pack()
+
+e=tk.Label(vnt,text="Id Producto", bg="gray",fg="white")
+e.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
+
+ed=tk.Entry(vnt)
+ed.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
 e0=tk.Label(vnt,text="Nombre del producto", bg="gray",fg="white")
 e0.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
@@ -51,10 +59,10 @@ ed3=tk.Entry(vnt)
 ed3.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
 boton0=tk.Button(vnt, text="Registar en \n inventario", fg="black")
-boton0.place(x=50,y=500)
+boton0.place(x=50,y=550)
 
 boton1=tk.Button(vnt, text="Actualizar el \n inventario", fg="black")
-boton1.place(x=200,y=500)
+boton1.place(x=200,y=550)
 
 boton2=tk.Button(vnt, text="Eliminar del \n inventario", fg="black")
 boton2.place(x=50,y=600)
@@ -62,7 +70,7 @@ boton2.place(x=50,y=600)
 boton3=tk.Button(vnt, text="Consultar en \n inventario", fg="black")
 boton3.place(x=200,y=600)
 
-boton4=tk.Button(vnt, text="Salir", fg="black")
+boton4=tk.Button(vnt, text="Salir", fg="black",command = cerrar)
 boton4.place(x=150,y=650)
 
 vnt.mainloop()
