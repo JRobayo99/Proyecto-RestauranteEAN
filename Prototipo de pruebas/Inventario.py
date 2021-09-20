9ab748fda763451610f5a3f65d719128b2467b1a
Julian Ramirez Robayo <jramire23664@universidadean.edu.co>
2021-09-19 20:37:50 -0500

    inventario

--- Prototipo de pruebas/Inventario.py
+++ Prototipo de pruebas/Inventario.py
@@ -5,15 +5,50 @@ from tkinter import messagebox
 import pymysql
 
 
-#cursor.execute("CREATE TABLE Inventario(id int PRIMARY KEY AUTO_INCREMENT NOT NULL, nombre_p VARCHAR(50), cantidad_c VARCHAR(50), clasifc_p VARCHAR(50), precio_u VARCHAR(50) NOT NULL)")
-bd=pymysql.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    db="inventario"    
-    )
+#cursor.execute("CREATE TABLE Lista(id int PRIMARY KEY AUTO_INCREMENT NOT NULL, nombre_p VARCHAR(50), cantidad_c VARCHAR(50), clasifc_p VARCHAR(50), precio_u VARCHAR(50) NOT NULL)")
 
-cursor = bd.cursor()
+def inserar_datos():
+    bd=pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        db="inventario"
+        )
+
+    cursor = bd.cursor()
+
+    sql="INSERT INTO Lista(id, nombre_p,cantidad_c,clasifc_p,precio_u)\
+          VALUES (NOT NULL,'{0}','{1}','{2}','{3}')".format(nomb.get(),cap.get(),cla.get(),preu.get())
+    try:
+         
+        cursor.execute(sql)
+        bd.commit()
+        messagebox.showinfo(message="Registro llevado con exito", title="Aviso")
+    except:
+         
+        bd.rollback()
+        messagebox.showinfo(message="Registro no  se llevo acabo", title="Aviso")
+        
+        bd.close()
+        
+def eliminar():
+    bd= pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        db="inventario"
+        )
+    cursor = bd.cursor()
+    sql = "DELETE FROM Lista WHERE nombre_p='"+nomb.get()+"'"
+    try:
+        cursor.execute(sql)
+        bd.commit()
+        messagebox.showinfo(message="Borrado exitoso", title="Aviso")
+    except:
+        bd.rollback()
+        messagebox.showinfo(message="No Eliminado",title="Aviso")
+
+        bd.close 
 
 def cerrar():
     vnt.destroy()
@@ -37,37 +72,37 @@ ed.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 e0=tk.Label(vnt,text="Nombre del producto", bg="gray",fg="white")
 e0.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
-ed0=tk.Entry(vnt)
-ed0.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
+nomb=tk.Entry(vnt)
+nomb.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
 e1=tk.Label(vnt,text="Cantidad del producto", bg="gray",fg="white")
 e1.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
-ed1=tk.Entry(vnt)
-ed1.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
+cap=tk.Entry(vnt)
+cap.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
 e2=tk.Label(vnt,text="Clasificación del producto", bg="gray",fg="white")
 e2.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
-ed2=tk.Entry(vnt)
-ed2.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
+cla=tk.Entry(vnt)
+cla.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
 e3=tk.Label(vnt,text="Precio por unidad", bg="gray",fg="white")
 e3.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
-ed3=tk.Entry(vnt)
-ed3.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
+preu=tk.Entry(vnt)
+preu.pack(padx=5, pady=5, ipadx=5, fill=tk.X)
 
-boton0=tk.Button(vnt, text="Registar en \n inventario", fg="black")
+boton0=tk.Button(vnt, text="Registar inventario", fg="black",command= inserar_datos)
 boton0.place(x=50,y=550)
 
-boton1=tk.Button(vnt, text="Actualizar el \n inventario", fg="black")
+boton1=tk.Button(vnt, text="Actualizar inventario", fg="black")
 boton1.place(x=200,y=550)
 
-boton2=tk.Button(vnt, text="Eliminar del \n inventario", fg="black")
+boton2=tk.Button(vnt, text="Eliminar del  inventario", fg="black",command= eliminar)
 boton2.place(x=50,y=600)
 
-boton3=tk.Button(vnt, text="Consultar en \n inventario", fg="black")
+boton3=tk.Button(vnt, text="Consultar inventario", fg="black")
 boton3.place(x=200,y=600)
 
 boton4=tk.Button(vnt, text="Salir", fg="black",command = cerrar)
